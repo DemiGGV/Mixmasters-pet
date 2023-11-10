d500172b0d549c7cc0be33783fff6552525d26c0
Heorhii Honcharov <118547076+DemiGGV@users.noreply.github.com>
2023-11-10 20:34:20 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/mixmasters">
+        <BrowserRouter basename="/Mixmasters-pet">
         <Toaster position="top-right" reverseOrder={false} />
           <App />
         </BrowserRouter>
