68ebc1815f27c1637688e6f76619591f8afadebd
Heorhii Honcharov <0019@ukr.net>
2023-11-19 13:25:40 +0100

    Rewriting and opt AuthSlice to Bilder notation

--- src/redux/auth/authSlice.js
+++ src/redux/auth/authSlice.js
@@ -25,117 +25,92 @@ const initialState = {
   error: null,
 };
 
+const handlePending = (state, _) => {
+  state.isRefreshing = true;
+  state.isLoading = true;
+};
+
+const handleRejected = (state, action) => {
+  state.error = action.payload;
+  state.isRefreshing = false;
+  state.isLoading = false;
+};
+// Register user
+const handleSignupUser = (state, action) => {
+  state.user = action.payload.user;
+  state.token = action.payload.token;
+  state.isLoggedIn = true;
+  state.isRefreshing = false;
+  state.isLoading = false;
+  state.error = null;
+};
+// Login user
+const handleSigninUser = (state, action) => {
+  state.user = action.payload.user;
+  state.token = action.payload.token;
+  state.isLoggedIn = true;
+  state.error = null;
+};
+// Logout user
+const handleSignoutUser = (state, _) => {
+  state.user = { name: null, birthdate: null, email: null };
+  state.token = null;
+  state.isLoggedIn = false;
+  state.error = null;
+};
+// Update user
+const handleUpdateUser = (state, action) => {
+  state.user.name = action.payload.name;
+  state.user.avatarURL = action.payload.avatarURL;
+  state.isRefreshing = false;
+  state.isLoading = false;
+  state.error = null;
+};
+// Refresh user
+const handleRefreshUser = (state, action) => {
+  state.user = action.payload;
+  state.isLoggedIn = true;
+  state.isRefreshing = false;
+  state.isLoading = false;
+  state.error = null;
+};
+// Subscribe user
+const handleSubscribeUser = (state, action) => {
+  state.user.subscription = action.payload.subscription;
+  state.isRefreshing = false;
+  state.isLoading = false;
+  state.error = null;
+};
+
 const authSlice = createSlice({
   name: 'auth',
   initialState,
-  extraReducers: {
-    // signUser
-
-    [signupUser.pending](state, _) {
-      state.isRefreshing = true;
-      state.isLoading = true;
-    },
-
-    [signupUser.fulfilled](state, action) {
-      state.user = action.payload.user;
-      state.token = action.payload.token;
-      state.isLoggedIn = true;
-      state.isRefreshing = false;
-      state.isLoading = false;
-      state.error = null;
-    },
-
-    [signupUser.rejected](state, action) {
-      state.error = action.payload;
-      state.isLoading = false;
-    },
-
-    // loginUser
-
-    [signinUser.fulfilled](state, action) {
-      state.user = action.payload.user;
-      state.token = action.payload.token;
-      state.isLoggedIn = true;
-      state.error = null;
-    },
-
-    [signinUser.rejected](state, action) {
-      state.error = action.payload;
-    },
-
-    // logoutUser
-    [updateUser.pending](state, _) {
-      state.isLoading = true;
-    },
-
-    [signoutUser.fulfilled](state) {
-      state.user = { name: null, birthdate: null, email: null };
-      state.token = null;
-      state.isLoggedIn = false;
-      state.error = null;
-    },
-
-    [signoutUser.rejected](state, action) {
-      state.error = action.payload;
-    },
-
-    // refreshUser
-
-    [refreshUser.pending](state, action) {
-      state.isRefreshing = true;
-      state.isLoading = true;
-    },
-
-    [refreshUser.fulfilled](state, action) {
-      state.user = action.payload;
-      state.isLoggedIn = true;
-      state.isRefreshing = false;
-      state.isLoading = false;
-      state.error = null;
-    },
-
-    [refreshUser.rejected](state, action) {
-      state.error = action.payload;
-      state.isLoading = false;
-    },
-    // updateUser
-    [updateUser.pending](state, _) {
-      state.isRefreshing = true;
-      state.isLoading = true;
-    },
-
-    [updateUser.fulfilled](state, action) {
-      state.user.name = action.payload.name;
-      state.user.avatarURL = action.payload.avatarURL;
-      state.isRefreshing = false;
-      state.isLoading = false;
-      state.error = null;
-    },
-
-    [updateUser.rejected](state, action) {
-      state.error = action.payload;
-      state.isRefreshing = false;
-      state.isLoading = false;
-    },
-
-    // subscribeUser
-    [subscribeUser.pending](state, _) {
-      state.isRefreshing = true;
-      state.isLoading = true;
-    },
-
-    [subscribeUser.fulfilled](state, action) {
-      state.user.subscription = action.payload.subscription;
-      state.isRefreshing = false;
-      state.isLoading = false;
-      state.error = null;
-    },
-
-    [subscribeUser.rejected](state, action) {
-      state.error = action.payload;
-      state.isRefreshing = false;
-      state.isLoading = false;
-    },
+  extraReducers: builder => {
+    builder
+      // Register user
+      .addCase(signupUser.pending, handlePending)
+      .addCase(signupUser.fulfilled, handleSignupUser)
+      .addCase(signupUser.rejected, handleRejected)
+      // Login user
+      .addCase(signinUser.pending, handlePending)
+      .addCase(signinUser.fulfilled, handleSigninUser)
+      .addCase(signinUser.rejected, handleRejected)
+      // Logout user
+      .addCase(signoutUser.pending, handlePending)
+      .addCase(signoutUser.fulfilled, handleSignoutUser)
+      .addCase(signoutUser.rejected, handleRejected)
+      // Update user
+      .addCase(updateUser.pending, handlePending)
+      .addCase(updateUser.fulfilled, handleUpdateUser)
+      .addCase(updateUser.rejected, handleRejected)
+      // Refresh user
+      .addCase(refreshUser.pending, handlePending)
+      .addCase(refreshUser.fulfilled, handleRefreshUser)
+      .addCase(refreshUser.rejected, handleRejected)
+      // Subscribe user
+      .addCase(subscribeUser.pending, handlePending)
+      .addCase(subscribeUser.fulfilled, handleSubscribeUser)
+      .addCase(subscribeUser.rejected, handleRejected);
   },
 });
 
